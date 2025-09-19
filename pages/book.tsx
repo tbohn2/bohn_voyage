8ef041c0ea65c0272fb943dca9b0c1ede54303b0
Tanner Bohn <tanner.bohn@truneighbor.co>
2025-09-19 08:53:28 -0700

    Updated handleTubeTypeChange

--- pages/book.tsx
+++ pages/book.tsx
@@ -12,19 +12,37 @@ export default function Book() {
     // 4: Pick Payment Method
     const [dateTime, setDateTime] = useState<Date | null>(new Date());
     const [availableTubes, setAvailableTubes] = useState<any[]>([]);
-    const [tubeTypes, setTubeTypes] = useState<any[]>([]);
+    const [tubeTypes, setTubeTypes] = useState<{
+        tubeTypeId: string,
+        quantity: number,
+        size: string
+    }[]>([]);
+    const [price, setPrice] = useState<number>(0);
 
     const uppercaseFirstLetter = (str: string) => {
         return str.charAt(0).toUpperCase() + str.slice(1);
     }
 
     const handleTubeTypeChange = (tubeTypeId: string, quantity: number) => {
+        if (isNaN(quantity)) return;
+
         const existingTubeType = tubeTypes.find((tube) => tube.tubeTypeId === tubeTypeId);
+        const tubePrice = availableTubes.find((tube) => tube.tube_type_id === tubeTypeId)?.price;
+
         if (existingTubeType) {
+            const oldPrice = existingTubeType.quantity * tubePrice;
+            const newPrice = quantity * tubePrice;
+            setPrice(price - oldPrice + newPrice);
+            console.log(price, oldPrice, newPrice);
+
             existingTubeType.quantity = quantity;
             setTubeTypes([...tubeTypes]);
+
         } else {
-            setTubeTypes([...tubeTypes, { tubeTypeId, quantity }]);
+            const tubeSize = availableTubes.find((tube) => tube.tube_type_id === tubeTypeId)?.size;
+            setTubeTypes([...tubeTypes, { tubeTypeId, quantity, size: tubeSize }]);
+            setPrice(price + quantity * tubePrice);
+            console.log(price, quantity * tubePrice);
         }
     }
 
@@ -50,6 +68,23 @@ export default function Book() {
             <h1>Book</h1>
 
             <div className="flex flex-col justify-center items-center gap-2 w-full max-w-md p-4">
+
+                {step > 1 && (
+                    <h2 className="text-lg font-medium">{dateTime?.toLocaleString('en-US', { weekday: 'long', month: 'long', day: 'numeric', year: 'numeric', hour: '2-digit', minute: '2-digit' })}</h2>
+                )}
+
+                {step > 2 && (
+                    <div className="flex flex-col justify-center items-center gap-2 w-full max-w-md p-4">
+                        <h2 className="text-lg font-medium">Total: ${price}</h2>
+                        {tubeTypes.map((tube) => (
+                            <div key={tube.tubeTypeId}>
+                                <h2 className="text-lg font-medium">{tube.size}</h2>
+                                <p className="text-sm text-gray-500">{tube.quantity} booked</p>
+                            </div>
+                        ))}
+                    </div>
+                )}
+
                 {step === 1 && (
                     <div className="flex flex-col justify-center items-center gap-2 w-full max-w-md p-4">
                         <label className="text-lg font-medium">Pick Date & Time</label>
@@ -77,7 +112,6 @@ export default function Book() {
 
                 {step === 2 && (
                     <div className="border border-indigo-500 rounded-lg p-4 flex flex-col justify-center items-center gap-2 w-full max-w-md">
-                        <h2 className="text-lg font-medium">{dateTime?.toLocaleString('en-US', { weekday: 'long', month: 'long', day: 'numeric', year: 'numeric', hour: '2-digit', minute: '2-digit' })}</h2>
                         <label className="text-lg font-medium">Pick Tube Types to book and quantity</label>
                         <div className="flex justify-center items-center gap-2 w-full max-w-md">
                             {availableTubes.map((tube) => (
@@ -88,9 +122,23 @@ export default function Book() {
                                 </div>
                             ))}
                         </div>
+                        <div className="text-lg font-medium">Total Price: ${price}</div>
                         <button onClick={() => setStep(3)} className="rounded-lg border border-indigo-500 bg-gray-50 p-3 text-indigo-900 font-semibold cursor-pointer">Next</button>
                     </div>
                 )}
+
+                {/* {step === 3 && (
+                    <div className="flex flex-col justify-center items-center gap-2 w-full max-w-md p-4">
+                        <label className="text-lg font-medium">Enter Customer Details</label>
+                        <input type="text" className="w-full px-1 bg-gray-50 text-indigo-900" placeholder="Name" />
+                        <input type="email" className="w-full px-1 bg-gray-50 text-indigo-900" placeholder="Email" />
+                        <input type="tel" className="w-full px-1 bg-gray-50 text-indigo-900" placeholder="Phone" />
+                    </div>
+                )}
+
+                {step !== 1 && (
+                    <button onClick={() => setStep(1)} className="rounded-lg border border-indigo-500 bg-gray-50 p-3 text-indigo-900 font-semibold cursor-pointer">Back</button>
+                )} */}
             </div>
         </div>
     );
